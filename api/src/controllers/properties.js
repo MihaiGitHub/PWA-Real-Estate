30a45467d36c17b6de9a03c523f9f080deb86615
Mihai Smarandache <mihai.sanfran@gmail.com>
2020-10-25 21:14:11 -0700

    Add agent object to properties

--- api/src/controllers/properties.js
+++ api/src/controllers/properties.js
@@ -15,7 +15,9 @@ const sequelize = new Sequelize(
 
 exports.list = async (req, res) => {
   try {
-    const properties = await Property.findAll({ include: [PropertyImages, User] });
+    const properties = await Property.findAll({
+      include: [PropertyImages, User],
+    });
 
     res.status(200).json(properties);
   } catch (e) {
@@ -37,6 +39,7 @@ exports.search = async (req, res) => {
 
     return Promise.all(
       results.map(async (property, index) => {
+        // add images to property
         const [imageresults, metadata] = await sequelize.query(
           "SELECT url, default_picture FROM properties_images WHERE pid = :pid ORDER BY default_picture DESC",
           {
@@ -47,6 +50,22 @@ exports.search = async (req, res) => {
         );
 
         property.PropertyImages = imageresults;
+
+        // add agent to property
+        const [agent, metadataAgent] = await sequelize.query(
+          "SELECT fname, lname, picture FROM users WHERE id = :uid",
+          {
+            replacements: {
+              uid: property.uid,
+            },
+          }
+        );
+
+        // turn array of object into an object before returning to frontend
+        const newObj = Object.assign({}, ...agent);
+
+        //  property.user = agent;
+        property.user = newObj;
 
         return property;
       })
